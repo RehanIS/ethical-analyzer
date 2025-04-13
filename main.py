1ec91088138bfe3aab2479a71b0f56b6b443a577
rehxns72 <rehxns72@gmail.com>
2025-04-13 16:53:06 +0530

    Added CORS support

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from models import ScenarioRequest, AnalysisResponse
 from cohere_handler import analyze_ethics
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can later restrict this to just your Netlify/Render URLs
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI(title="Ethical Scenario Analyzer")
 
